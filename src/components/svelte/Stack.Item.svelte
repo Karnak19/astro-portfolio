d1604a78f8fd6b3b2486e789ae0396efac855326
Basile Vernouillet <basile64.v@gmail.com>
2022-09-30 00:11:43 +0200

    init

---
+++ src/components/svelte/Stack.Item.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let title: string;
+	export let d: string;
+	export let color: string;
+	export let bg: string;
+	export let desc: string;
+</script>
+
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<div class=" w-full lg:w-1/5 lg:h-80 group lg:hover:w-1/2 transition-all duration-700">
+	<article
+		class="bg-white/30 hover:bg-white/60 overflow-hidden py-4 lg:py-0 transition-colors duration-700 backdrop-blur-xl h-full flex flex-row px-1 lg:px-0 lg:flex-col gap-2 items-center border-2 border-white/20 rounded-md">
+		<div
+			class="hidden lg:block overflow-hidden h-1/4 -translate-y-20 group-hover:translate-y-0 transition-transform delay-500">
+			<h1 class="text-3xl font-prompt">
+				{title}
+			</h1>
+		</div>
+		<div class={`h-1/4 text-white`}>
+			<svg
+				role="img"
+				class={`h-20 w-20 ${color} lg:text-white lg:group-hover:${color} ${bg} lg:bg-transparent lg:group-hover:${bg} transition-colors delay-500 p-2 fill-current rounded-full`}
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"><title>{title}</title><path {d} /></svg>
+		</div>
+		<div class="overflow-hidden flex items-center h-full px-4">
+			<p
+				class="text-xs text-justify lg:translate-y-80 lg:group-hover:translate-y-0 transition-transform ease-out delay-700">
+				{desc}
+			</p>
+		</div>
+	</article>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: [
+    "lg:group-hover:text-react",
+    "lg:group-hover:text-nestjs",
+    "lg:group-hover:text-typescript",
+    "lg:group-hover:text-white",
+    "lg:group-hover:text-black",
+    "lg:group-hover:text-prisma",
+    "lg:group-hover:text-graphql",
+    "lg:group-hover:bg-typescript",
+    "lg:group-hover:bg-white",
+    "lg:group-hover:bg-black",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        open: ["Open Sans Condensed", "sans-serif"],
+        prompt: ["Prompt", "sans-serif"],
+      },
+      colors: {
+        react: "#61DAFB",
+        nestjs: "#E0234E",
+        typescript: "#3178C6",
+        graphql: "#E10098",
+        prisma: "#2D3748",
+      },
+    },
+  },
+  plugins: [],
+};
